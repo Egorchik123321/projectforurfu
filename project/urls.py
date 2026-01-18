b6fcb3860872ae466cfcb492afca41e312db8958
Егор Чепуров <zhora.chepurov@mail.ru>
2026-01-18 20:14:55 +0500

    Update

--- project/urls.py
+++ project/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static # pyright: ignore[reportMissingModule
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('content.urls')),
-    path('api/', include('content.api_urls')),  # Добавьте эту строку
+    path('api/', include('content.api_urls')),  
 ]
 
 if settings.DEBUG:
